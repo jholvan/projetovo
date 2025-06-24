832d414868a9417e25294748bc01c6974e582d00
jholvan <jholvan98@gmail.com>
2025-06-24 10:31:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -371,6 +371,11 @@ document.getElementById('baixarResumoBtn').addEventListener('click', function() 
   const nomeArquivo = `Resumo-${tituloCategoria.replace(/\s/g,'')}-${mesSelecionado}.pdf`;
   doc.save(nomeArquivo);
 import { neon } from '@netlify/neon';
-const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
+const sql = neon(); 
+postgresql://neondb_owner:npg_J7yhYnj6XEUO@ep-royal-rice-a5u30nbx.us-east-2.aws.neon.tech/neondb?sslmode=require
+postgresql://neondb_owner:npg_J7yhYnj6XEUO@ep-royal-rice-a5u30nbx.us-east-2.aws.neon.tech/neondb?sslmode=require
+postgresql://neondb_owner:npg_J7yhYnj6XEUO@ep-royal-rice-a5u30nbx.us-east-2.aws.neon.tech/neondb?sslmode=require
+postgresql://neondb_owner:npg_J7yhYnj6XEUO@ep-royal-rice-a5u30nbx.us-east-2.aws.neon.tech/neondb?sslmode=require
+postgresql://neondb_owner:npg_J7yhYnj6XEUO@ep-royal-rice-a5u30nbx.us-east-2.aws.neon.tech/neondb?sslmode=require
 const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
 });
